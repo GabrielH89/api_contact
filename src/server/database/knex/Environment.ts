abba12c9a8620e18b01f32e899aea7717245c847
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2024-01-03 21:24:05 -0300

    Updated Environment.ts file and server.ts

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,3 +1,49 @@
+/*import { Knex } from 'knex';
+import path from 'path';
+
+
+export const development: Knex.Config = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  pool: {
+    afterCreate: (connection: any, done: Function) => {
+      connection.run('PRAGMA foreign_keys = ON');
+      done();
+    }
+  }
+};
+
+export const test: Knex.Config = {
+  ...development,
+  connection: ':memory:',
+};
+
+export const production: Knex.Config = {
+  //...development,
+  client: 'mysql',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: {
+   host: 'localhost',
+   user: 'root',
+   database: 'api_contact',
+   password: 'gm7102@L',
+  },
+};*/
+
 import { Knex } from 'knex';
 import path from 'path';
 
@@ -30,3 +76,6 @@ export const test: Knex.Config = {
 export const production: Knex.Config = {
   ...development,
 };
+
+
+
